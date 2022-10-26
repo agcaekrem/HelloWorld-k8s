92b3c456d45816c44439fc25eb4ccc24b94f5f30
agcaekrem <agcaa.ekrem@gmail.com>
2022-10-26 23:24:08 +0300

    ilk commit

---
+++ src/main/java/com/example/helloworldk8s/HelloWorldK8sApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworldk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldK8sApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldK8sApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/helloworldk8s/HelloWorldK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworldk8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldK8sApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
